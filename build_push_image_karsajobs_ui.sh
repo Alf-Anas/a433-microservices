119ec35c50b6e7d045bf13901039d8216828524f
Alf-Anas <alfadila.anas@gmail.com>
2024-08-21 17:21:53 +0700

    add build sh

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build Docker image
+docker build -t ghcr.io/alf-anas/karsajobs-ui:latest .
+
+# Login to GitHub
+echo "$TOKEN_GITHUB" | docker login ghcr.io -u alf-anas --password-stdin
+
+# Push Docker image to GitHub Packages
+docker push ghcr.io/alf-anas/karsajobs-ui:latest
